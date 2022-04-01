ac671a4b782dd05c5d704cde1fea4b12c6af1ef0
jangsujeong <sujeongjang1214@gmail.com>
2022-04-01 21:54:45 +0900

    webcam connect 1

--- mask_project.py
+++ mask_project.py
@@ -29,3 +29,26 @@ key = cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
+
+is_readed, frame = webcam.read()
+
+# # 1. 웹캠 연결하기
+
+webcam = cv2.VideoCapture(0)#기본카메라 0번 사용
+if not webcam.isOpened():
+    raise Exception("카메라 읎음")
+
+
+#프레임 받아오기
+ret, frame = webcam.read() #2개의 리턴값을 튜플로 반환함.
+if not ret:
+    raise Exception("캡쳐가 없음")
+
+faces, confidences = cv.detect_face(frame) #이미지에서 얼굴 위치, 얼굴일 확률 받아오기
+print(faces[0])#좌표 4지점 
+print(confidences)
+
+# 이미지 저장하기
+start_x, start_y, end_x, end_y = faces
+
+
