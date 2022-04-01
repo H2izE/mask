b46043c717d134324d4b47784d0fdfbf6c9c85bc
jangsujeong <sujeongjang1214@gmail.com>
2022-04-01 11:22:11 +0900

    image

--- mask_project.py
+++ mask_project.py
@@ -13,4 +13,19 @@
 #     name: python3
 # ---
 
-import 
+import cv2
+import cvlib as cv
+
+image = cv2.imread('sample.png')
+faces, confidences = cv.detect_face(image)
+
+for (x,y,x2,y2), conf in zip(faces, confidences):
+    cv2.putText(image, str(conf), (x,y-10), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0),1)
+    cv2.rectangle(image, (x,y), (x2,y2), (0,255,0), 2)
+
+cv2.imshow('image',image)
+
+key = cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
